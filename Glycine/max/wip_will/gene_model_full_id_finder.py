e2a100c0145cdd74568bc4b77d618414ce7e8b3e
William Hardison <64339265+willhardison@users.noreply.github.com>
2023-06-13 20:38:33 +0100

    Update gene_model_full_id_finder.py

--- Glycine/max/wip_will/gene_model_full_id_finder.py
+++ Glycine/max/wip_will/gene_model_full_id_finder.py
@@ -1,7 +1,7 @@
 import yaml
 
 
-def gmfi_finder(tsv_path, yaml_path, output_path):
+def gene_model_full_id_finder(tsv_path, yaml_path, output_path):
     """# finds gene_model_full_id in tsv file based on gene_model_pub_name and adds it to the yaml file"""
     with open(tsv_path, 'r') as tsv_file:
         # Open the YAML file and load its contents as separate Python objects
@@ -25,7 +25,7 @@ def gmfi_finder(tsv_path, yaml_path, output_path):
                 yaml.dump_all(output_data, outfile)
 
 
-def get_gmfi_dictionary(tsv_path, yaml_path):
+def get_gene_model_full_id_dictionary(tsv_path, yaml_path):
     """returns a dictionary of gene_model_pub_name:gene_model_full_id"""
     with open(tsv_path, 'r') as tsv_file:
         # Open the YAML file and load its contents as separate Python objects
@@ -45,16 +45,16 @@ def get_gmfi_dictionary(tsv_path, yaml_path):
             return output_data
 
 
-GLYMA_YAML_PATH = 'wip_steven/Oellrich_Walls_2015_glyma.traits.yml'
+GLYMA_YAML_PATH = 'FILE.traits.yml'
 GLYMA_TSV_PATH = 'Glycine.pan3.YWTW.clust.tsv'
 
-
-MEDICAGO_YAML_PATH = 'wip_steven/Oellrich_Walls_2015_medtr.medtr.traits.yml'
-MEDICAGO_TSV_PATH = 'Medicago.pan1.XXQ6.clust.tsv'
-
-
 # find the gene model full id in the tsv file for GLYMA
-gmfi_finder(GLYMA_TSV_PATH, GLYMA_YAML_PATH, 'g_outfile.yml')
+gene_model_full_id_finder(GLYMA_TSV_PATH, GLYMA_YAML_PATH, 'g_outfile.yml')
+
 
 # find the gene model full id in the tsv file for Medicago
-gmfi_finder(MEDICAGO_TSV_PATH, MEDICAGO_YAML_PATH, 'm_outfile.yml')
+
+# MEDICAGO_YAML_PATH = 'wip_steven/Oellrich_Walls_2015_medtr.medtr.traits.yml'
+# MEDICAGO_TSV_PATH = 'Medicago.pan1.XXQ6.clust.tsv'
+
+# gmfi_finder(MEDICAGO_TSV_PATH, MEDICAGO_YAML_PATH, 'm_outfile.yml')
